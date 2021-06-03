b1da5b6e708be3e2b2566634fa2ce783792b3f68
Kaushik Guha <kguha@mail.sjsm.org>
2021-06-02 19:58:45 -0500

    ready to deploy

--- models.py
+++ models.py
@@ -1,9 +1,16 @@
+import os
+from playhouse.db_url import connect
+
 from peewee import *
 import datetime
 from flask_login import UserMixin
 
 # DATABASE=SqliteDatabase('pmt.sqlite')
-DATABASE=PostgresqlDatabase('pmt', user='kguha')
+# DATABASE=PostgresqlDatabase('pmt', user='kguha')
+
+DATABASE = connect(os.environ.get('DATABASE_URL') or 'sqlite:///pmt.sqlite')
+# Connect to the database URL defined in the environment, falling
+# back to a local Sqlite database if no database URL is specified.
 
 #Our Model
 class User(UserMixin, Model):
